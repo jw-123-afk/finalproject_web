f631158da7eeae8c11db2e606ffdd5c6c76cf668
jw-123-afk <lowjw3666@gmail.com>
2025-06-26 13:58:19 +0800

    Add files via upload

--- web_finalproject/src/main/java/com/stiwk2124/project/springboot/backend/repository/UserRepository.java
+++ web_finalproject/src/main/java/com/stiwk2124/project/springboot/backend/repository/UserRepository.java
@@ -6,4 +6,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
+
+    User findByNameAndEmailAndPhone(String name, String email, String phone);
+
 }
